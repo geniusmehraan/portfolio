4c1f58041cd958e2b06c26ce6686e5cc4b7860ef
Genuis mehraan <mehdiayaan786@gmail.com>
2025-03-30 09:56:00 +0530

    changed basics

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+import ayaanImage from "../../../public/2 ayaan.jpg"
+const Navbar = () => {
+  return (
+    <div className="w-full h-[65px] flex bg-[#1A1A2E] items-center justify-end px-14">
+      <h1 className=" px-5  flex items-center justify-center rounded-full text-white text-lg  font-bold"><span className="text-red-600  font-extrabold">A</span>YAAN</h1>
+    </div>
+  )
+}
+
+export default Navbar
